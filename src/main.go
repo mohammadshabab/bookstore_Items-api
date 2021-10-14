bc76a08174e1ff28fafd679343627f2c2e545145
Mohammad Shabab <shababkhan04@gmail.com>
2021-10-14 16:48:11 +0530

    main

--- src/main.go
+++ src/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"os"
-
 	"github.com/mohammadshabab/bookstore_items-api/src/app"
 )
 
 func main() {
-	os.Setenv("LOG_LEVEL", "info")
+	//	os.Setenv("LOG_LEVEL", "info")
 	app.StartApplication()
 }
